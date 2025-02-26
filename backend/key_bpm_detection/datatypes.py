27503af04f7398ca3a36c2da62d866ae379ccb2f
Lawjay Lee <lawjaylee1@icloud.com>
2025-02-25 16:47:01 -0800

    Adds backend dir, adds datatype for song keys

---
+++ backend/key_bpm_detection/datatypes.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+
+class Song_Key(Enum):
+    C_MAJOR = "C Major"
+    C_MINOR = "C Minor"
+    C_SHARP_MAJOR = "C# Major"
+    C_SHARP_MINOR = "C# Minor"
+    D_MAJOR = "D Major"
+    D_MINOR = "D Minor"
+    D_SHARP_MAJOR = "D# Major"
+    D_SHARP_MINOR = "D# Minor"
+    E_MAJOR = "E Major"
+    E_MINOR = "E Minor"
+    F_MAJOR = "F Major"
+    F_MINOR = "F Minor"
+    F_SHARP_MAJOR = "F# Major"
+    F_SHARP_MINOR = "F# Minor"
+    G_MAJOR = "G Major"
+    G_MINOR = "G Minor"
+    G_SHARP_MAJOR = "G# Major"
+    G_SHARP_MINOR = "G# Minor"
+    A_MAJOR = "A Major"
+    A_MINOR = "A Minor"
+    A_SHARP_MAJOR = "A# Major"
+    A_SHARP_MINOR = "A# Minor"
+    B_MAJOR = "B Major"
+    B_MINOR = "B Minor"
