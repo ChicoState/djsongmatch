5be5cc553a73e2048c8bbe1487eb2f0fc03db0ea
Lawjay Lee <lawjaylee1@icloud.com>
2025-02-27 16:00:22 -0800

    Adds search bar with dummy data

--- djsongmatch/src/app/page.tsx
+++ djsongmatch/src/app/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+import { AutoComplete, Option } from "@/components/ui/autocomplete";
 import "./globals.css";
+
+// mock data, delete later
+const options: Option[] = [
+  { value: "Song 1", label: "Song 1" },
+  { value: "Song 2", label: "Song 2" },
+  { value: "Song 3", label: "Song 3" },
+];
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 justify-center items-center h-screen">
-      <h1 className="text-6xl text-white">DJ Song Match!</h1>
-      <p className="text-2xl text-white">Begin by searching for a song.</p>
+      <h1 className="text-6xl text-foreground">DJ Song Match!</h1>
+      <p className="text-2xl text-foreground">Begin by searching for a song.</p>
+      <AutoComplete options={options} emptyMessage="No songs found." />
     </div>
   );
 }
