eecaeaef086e984c079263974b9b8db253128fba
Sergio Martinez <srmartinezbarajas@csuchico.edu>
2025-03-03 12:41:35 -0800

    Added about us file

---
+++ src/app/about-page/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div className="flex flex-col gap-4 justify-center items-center h-screen">
+      <h1 className="text-6xl text-foreground">About Us</h1>
+      <p className="text-2xl text-foreground">This is the about us section of our app.</p>
+      <p className="text-2xl text-foreground">Adding names here to make sure environment is working: Sergio</p>
+    </div>
+  );
+}
