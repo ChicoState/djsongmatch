095c08dc005e35f1d08d73b080c33d14c7333b70
Rodrigo Lopez <lopezr56h@gmail.com>
2025-03-03 12:47:12 -0800

    Added my name

--- src/app/about-page/page.tsx
+++ src/app/about-page/page.tsx
@@ -4,6 +4,7 @@ export default function AboutPage() {
       <h1 className="text-6xl text-foreground">About Us</h1>
       <p className="text-2xl text-foreground">This is the about us section of our app.</p>
       <p className="text-2xl text-foreground">Adding names here to make sure environment is working: Sergio</p>
+      <p className = "text-2xl text-foreground">Rodrigo Lopez</p>
     </div>
   );
 }
