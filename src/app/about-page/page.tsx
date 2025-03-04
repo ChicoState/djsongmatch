4e556b316c5caedf5a25a3f6be19d62aa4f932f8
Anmol <anmolvirdi20@gmail.com>
2025-03-03 19:36:02 -0800

    Added name

--- src/app/about-page/page.tsx
+++ src/app/about-page/page.tsx
@@ -3,7 +3,12 @@ export default function AboutPage() {
     <div className="flex flex-col gap-4 justify-center items-center h-screen">
       <h1 className="text-6xl text-foreground">About Us</h1>
       <p className="text-2xl text-foreground">This is the about us section of our app.</p>
+<<<<<<< HEAD
       <p className="text-2xl text-foreground">Adding names here to make sure environment is working: Sergio, Anmol</p>
+=======
+      <p className="text-2xl text-foreground">Adding names here to make sure environment is working: Sergio</p>
+      <p className = "text-2xl text-foreground">Rodrigo Lopez</p>
+>>>>>>> 095c08dc005e35f1d08d73b080c33d14c7333b70
     </div>
   );
 }
