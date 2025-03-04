7f3645622a94ecd4ef45425bbce18cc0e896134f
Madison Webb <madisonewebb28@gmail.com>
2025-03-03 21:31:42 -0800

    Added name, resolved merge conflict

--- src/app/about-page/page.tsx
+++ src/app/about-page/page.tsx
@@ -3,15 +3,7 @@ export default function AboutPage() {
     <div className="flex flex-col gap-4 justify-center items-center h-screen">
       <h1 className="text-6xl text-foreground">About Us</h1>
       <p className="text-2xl text-foreground">This is the about us section of our app.</p>
-<<<<<<< HEAD
-      <p className="text-2xl text-foreground">Adding names here to make sure environment is working: Sergio, Anmol</p>
-=======
-      <p className="text-2xl text-foreground">Adding names here to make sure environment is working: Sergio</p>
-      <p className = "text-2xl text-foreground">Rodrigo Lopez</p>
-<<<<<<< HEAD
->>>>>>> 095c08dc005e35f1d08d73b080c33d14c7333b70
-=======
->>>>>>> 095c08dc005e35f1d08d73b080c33d14c7333b70
+      <p className="text-2xl text-foreground">Adding names here to make sure environment is working: Sergio, Anmol, Rodrigo Lopez, Madison Webb</p>
     </div>
   );
 }
