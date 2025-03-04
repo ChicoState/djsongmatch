c303e02839b726595167de371a62e272f869dc82
Lawjay Lee <lawjaylee1@icloud.com>
2025-03-04 15:50:51 -0800

    Moved form to another file, wrapped Homepage in ClientQuery for Tanstack Query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,27 @@
-import { AutoComplete, Option } from "@/components/ui/autocomplete";
-import { musicData } from "@/db/schema";
-import SongTable from "@/components/ui/songtable";
+"use client";
 import "./globals.css";
+import SongForm from "@/components/ui/SongForm";
+import SongTable from "@/components/ui/songtable";
+import { Song } from "@/db/schema";
 
-import "dotenv/config";
-import { drizzle } from "drizzle-orm/libsql";
-import { sql } from "drizzle-orm";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
-const db = drizzle("file:./assets/ClassicHit.db");
-
-async function SongsAutoComplete() {
-  const songs = await db
-    .select()
-    .from(musicData)
-    .orderBy(sql`RANDOM()`)
-    .limit(10);
-
-  const options: Option[] = songs.map((song) => {
-    return {
-      value: song.artist + " - " + song.title,
-      label: song.artist + " - " + song.title,
-      key: song.songId.toString(),
-    };
-  });
-  return <AutoComplete options={options} emptyMessage="" />;
-}
+const queryClient = new QueryClient();
 
 export default function Home() {
+  const [tableData, setTableData] = useState<Song[]>([]);
+
   return (
-    <div className="flex flex-row gap-24 h-full">
-      {/* Left side content */}
-      <div className="flex flex-col gap-4 grow-[1]">
-        <p className="text-2xl text-center text-foreground">
-          Search for a song!
-        </p>
-        <SongsAutoComplete />
-        <button className="self-center p-4 w-24 bg-green-200">Generate</button>
+    <QueryClientProvider client={queryClient}>
+      <div className="flex flex-row gap-24 h-full">
+        {/* Left side content */}
+        <SongForm onFetchDataAction={setTableData} />
+        {/* Right side content */}
+        <div className="h-full grow-[4]">
+          <SongTable data={tableData} />
+        </div>
       </div>
-      {/* Right side content */}
-      <div className="h-full grow-[4]">
-        <SongTable />
-      </div>
-    </div>
+    </QueryClientProvider>
   );
 }
