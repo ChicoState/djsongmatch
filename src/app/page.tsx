d2f9b5859544fac1e07784796d1528a7816487a8
riley-1995 <rileydenn@gmail.com>
2025-03-03 18:54:21 -0800

    Update page.tsx to add a blank table for before the user hits the generate button.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,6 +26,8 @@ async function SongsAutoComplete() {
 }
 
 export default function Home() {
+  const nRows = Array.from({ length: 10 }, (_, index) => index);
+
   return (
     <div className="flex flex-row gap-24 h-full">
       <div className="flex flex-col gap-4 grow-[1]">
@@ -35,7 +37,38 @@ export default function Home() {
         <SongsAutoComplete />
         <button className="self-center p-4 w-24 bg-green-200">Generate</button>
       </div>
-      <p className="h-full bg-red-50 grow-[4]">Table goes here</p>
+      <div className="h-full grow-[4]">
+        <table className="w-full border-collapse">
+          {/* Table column width */}
+          <colgroup>
+              <col className="w-5/12" /> {/* Title: 5/12 of space */}
+              <col className="w-4/12" /> {/* Artist: 4/12 of space */}
+              <col className="w-1/12" /> {/* BPM: 1/12 of space */}
+              <col className="w-2/12" /> {/* Key: 2/12 of space */}
+            </colgroup>
+          {/* Table header */}
+          <thead>
+            <tr className="bg-gray-100">
+              <th className="p-3 text-left border">Title</th>
+              <th className="p-3 text-left border">Artist</th>
+              <th className="p-3 text-left border">BPM</th>
+              <th className="p-3 text-left border">Key</th>
+            </tr>
+          </thead>
+          {/* Table body */}
+          <tbody>
+            {/* Empty rows to show the blank table structure */}
+            {Array(10).fill(0).map((_, index) => (
+              <tr key={index} className="hover:bg-gray-50">
+                <td className="p-3 border text-gray-300">-</td>
+                <td className="p-3 border text-gray-300">-</td>
+                <td className="p-3 border text-gray-300">-</td>
+                <td className="p-3 border text-gray-300">-</td>
+              </tr>
+            ))}
+            </tbody>
+          </table>
+      </div>
     </div>
   );
 }
