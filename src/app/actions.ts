28cc7b461d68f24a179825b0565312f3564ca178
Lawjay Lee <lawjaylee1@icloud.com>
2025-03-04 16:08:57 -0800

    Adds song data type to server action

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,14 +2,14 @@
 // entire file uses server ACTIONS, 
 // do NOT export components in this file, only functions
 
-import { musicData } from "@/db/schema";
+import { musicData, Song } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/libsql";
 
 const db = drizzle("file:./assets/ClassicHit.db");
 
 export async function getSong(songId: number) {
-    const song = await db
+    const song: Song[] = await db
         .select()
         .from(musicData)
         .where(
