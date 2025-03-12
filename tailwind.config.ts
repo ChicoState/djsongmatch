6452156fa9c9c4d0963823ff74905fe518571b79
Lawjay Lee <lawjaylee1@icloud.com>
2025-03-11 19:28:25 -0700

    Adds shadcn ui downdown

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,20 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        table_border: " #6B7280 ",
-        table_text: "gray-500"
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                bg: "#fff",
+                fg: "#000",
+                table_border: "#6B7280",
+                table_text: "gray-500"
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
