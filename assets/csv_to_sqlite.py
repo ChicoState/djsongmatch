388a9f907edae446861f66c9e1a67c5abe2a1e59
riley-1995 <rileydenn@gmail.com>
2025-03-13 18:08:53 -0700

    Update csv_to_sqlite.py to include feature normalization. Updates ClassicHit.db and adds normalized_ClassicHit.csv

--- assets/csv_to_sqlite.py
+++ assets/csv_to_sqlite.py
@@ -1,19 +1,39 @@
 import csv
 import sqlite3
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import os
 
-CSV_FILE = "ClassicHit.csv"
-DB_FILE = "ClassicHit.db"
+CSV_FILE = os.path.join(os.path.dirname(__file__), "ClassicHit.csv")
+CSV_NORMALIZED_FILE = os.path.join(os.path.dirname(__file__), "normalized_ClassicHit.csv")
+DB_FILE = os.path.join(os.path.dirname(__file__), "ClassicHit.db")
 
-all_rows = []
-with open(CSV_FILE, "r") as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        all_rows.append(row)
+''' 
+Function to normalize features ['Danceability', 'Energy', 'Loudness',
+'Speechiness', 'Acousticness', 'Instrumentalness', 'Liveness', 'Valence'] 
+to values between 0 and 1.
+Returns a new DataFrame that contains the same feature columns as the original, but 
+replaces the values of the above features with normalized values.
+'''
+def normalize_features(df, save_to_csv=False):
+    features = ['Danceability', 'Energy', 'Loudness','Speechiness', 'Acousticness', 
+            'Instrumentalness', 'Liveness', 'Valence']
+    scaler = MinMaxScaler()
+    df2 = df.copy()
+    df2['Loudness_dB'] = df['Loudness'] # Keep the original Loudness column, which is in dB, but rename it
+    df2[features] = scaler.fit_transform(df[features])
 
+    if save_to_csv:
+        df2.to_csv(os.path.join(CSV_NORMALIZED_FILE), index=False)
 
-def create_database():
+    return df2
+
+def create_database(df):
     conn = sqlite3.connect(DB_FILE)
     c = conn.cursor()
+
+    # Drop the existing table (if it exists)
+    c.execute("DROP TABLE IF EXISTS music_data")
 
     table_query = """
         CREATE TABLE IF NOT EXISTS music_data (
@@ -26,7 +46,8 @@ def create_database():
         Danceability FLOAT,
         Energy FLOAT,
         `Key` INT, -- Musical key as integer (e.g., 0 = C, 1 = C#, etc.)
-        Loudness FLOAT,
+        Loudness FLOAT, -- Normalized Loudness
+        Loudness_dB FLOAT, -- Original Loudness in dB
         Mode INT, -- Mode: 0 = Minor, 1 = Major
         Speechiness FLOAT,
         Acousticness FLOAT,
@@ -41,11 +62,12 @@ def create_database():
 
     c.execute(table_query)
 
-    for row in all_rows:
+    for _, row in df.iterrows():
         c.execute(
             """
-            INSERT INTO music_data (Title, Artist, Year, Duration, Time_Signature, Danceability, Energy, `Key`, Loudness, Mode, Speechiness, Acousticness, Instrumentalness, Liveness, Valence, Tempo, Popularity, Genre)
-            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+            INSERT INTO music_data (Title, Artist, Year, Duration, Time_Signature, Danceability, Energy, `Key`, Loudness, 
+            Loudness_dB, Mode, Speechiness, Acousticness, Instrumentalness, Liveness, Valence, Tempo, Popularity, Genre)
+            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
             """,
             (
                 row["Track"], # renamed to Title
@@ -57,6 +79,7 @@ def create_database():
                 row["Energy"],
                 row["Key"],
                 row["Loudness"],
+                row["Loudness_dB"],
                 row["Mode"],
                 row["Speechiness"],
                 row["Acousticness"],
@@ -69,6 +92,7 @@ def create_database():
             ),
         )
     conn.commit()
+    conn.close()
 
 
 def print_first_rows(n: int=5):
@@ -78,9 +102,12 @@ def print_first_rows(n: int=5):
     rows = c.fetchall()
     for row in rows:
         print(row)
+    conn.close()
 
 def main():
-    create_database()
+    df = pd.read_csv(CSV_FILE)
+    df_normal = normalize_features(df, save_to_csv=True) # Normalize the features
+    create_database(df_normal) # Create the database and insert the data
     print_first_rows()
 
 if __name__ == "__main__":
