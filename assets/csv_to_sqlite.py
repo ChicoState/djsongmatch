c78b8c9a126e04a3f3d373d8830b693ebdf13528
riley-1995 <rileydenn@gmail.com>
2025-03-13 22:55:51 -0700

    Add camelot key mapping column to database, remove normalize_ClassicHit.csv and replace it with Processed_ClassicHit.csv, which includes camelot key column.

--- assets/csv_to_sqlite.py
+++ assets/csv_to_sqlite.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import MinMaxScaler
 import os
 
 CSV_FILE = os.path.join(os.path.dirname(__file__), "ClassicHit.csv")
-CSV_NORMALIZED_FILE = os.path.join(os.path.dirname(__file__), "normalized_ClassicHit.csv")
+PROCESSED_CSV_FILE = os.path.join(os.path.dirname(__file__), "Processed_ClassicHit.csv")
 DB_FILE = os.path.join(os.path.dirname(__file__), "ClassicHit.db")
 
 ''' 
@@ -15,7 +15,7 @@ to values between 0 and 1.
 Returns a new DataFrame that contains the same feature columns as the original, but 
 replaces the values of the above features with normalized values.
 '''
-def normalize_features(df, save_to_csv=False):
+def normalize_features(df):
     features = ['Danceability', 'Energy', 'Loudness','Speechiness', 'Acousticness', 
             'Instrumentalness', 'Liveness', 'Valence']
     scaler = MinMaxScaler()
@@ -23,10 +23,54 @@ def normalize_features(df, save_to_csv=False):
     df2['Loudness_dB'] = df['Loudness'] # Keep the original Loudness column, which is in dB, but rename it
     df2[features] = scaler.fit_transform(df[features])
 
-    if save_to_csv:
-        df2.to_csv(os.path.join(CSV_NORMALIZED_FILE), index=False)
-
     return df2
+
+'''
+Function to add a new column 'Camelot_Key' to the DataFrame based on the 'Key' and 'Mode' columns.
+The Camelot key is a system used by DJs to mix tracks harmonically. They are typically represented as
+numbers from 1 to 12, with a letter indicating the mode (A for minor, B for major).
+This mapping uses the values 1-12 for the minor keys and 13-24 for the major keys, while still maintaining 
+relative order.
+Ex: Camelot Key 1A is represented as 1; Camelot Key 1B is represented as 13. Camelot Key 2A is represented as 2;
+Camelot Key 2B is represented as 14, etc.
+Returns the DataFrame with the new 'Camelot_Key' column.
+'''
+def add_camelot_key(df):
+    # Mapping of musical keys to Camelot keys based on mode (minor/major)
+    musical_key_to_camelot = {
+        # Major keys (Mode = 1)
+        0: 20,      # 8B ~ C
+        1: 15,      # 3B ~ C# / Db
+        2: 22,      # 10B ~ D
+        3: 17,      # 5B ~ D# / Eb
+        4: 24,      # 12B ~ E
+        5: 19,      # 7B ~ F
+        6: 14,      # 2B ~ F# / Gb
+        7: 21,      # 9B ~ G
+        8: 16,      # 4B ~ G# / Ab
+        9: 23,      # 11B ~ A
+        10: 18,     # 6B ~ A# / Bb
+        11: 13,     # 1B ~ B
+
+        # Minor keys (Mode = 0)
+        12: 5,      # 5A ~ C
+        13: 12,     # 12A ~ C# / Db
+        14: 7,      # 7A ~ D
+        15: 2,      # 2A ~ D# / Eb
+        16: 9,      # 9A ~ E
+        17: 4,      # 4A ~ F
+        18: 11,     # 11A ~ F# / Gb
+        19: 6,      # 6A ~ G
+        20: 1,      # 1A ~ G# / Ab
+        21: 8,      # 8A ~ A
+        22: 3,      # 3A ~ A# / Bb
+        23: 10,     # 10A ~ B
+    }
+    
+    # Map KeyMode to Camelot keys
+    df['Camelot_Key'] = (df['Key'] + (df['Mode'] * 12)).map(musical_key_to_camelot)
+    
+    return df
 
 def create_database(df):
     conn = sqlite3.connect(DB_FILE)
@@ -36,25 +80,26 @@ def create_database(df):
     c.execute("DROP TABLE IF EXISTS music_data")
 
     table_query = """
-        CREATE TABLE IF NOT EXISTS music_data (
+        CREATE TABLE music_data (
         SongID INTEGER PRIMARY KEY AUTOINCREMENT,
         Title VARCHAR(255),
         Artist VARCHAR(255),
         Year INT, -- Year of release (assuming integer year)
         Duration INT, -- Duration of the track in milliseconds
         Time_Signature INT,
+        `Key` INT, -- Musical key as integer (e.g., 0 = C, 1 = C#, etc.)
+        Mode INT, -- Mode: 0 = Minor, 1 = Major
+        Camelot_Key INT, -- Camelot key as integer
+        Tempo FLOAT,
         Danceability FLOAT,
         Energy FLOAT,
-        `Key` INT, -- Musical key as integer (e.g., 0 = C, 1 = C#, etc.)
         Loudness FLOAT, -- Normalized Loudness
         Loudness_dB FLOAT, -- Original Loudness in dB
-        Mode INT, -- Mode: 0 = Minor, 1 = Major
         Speechiness FLOAT,
         Acousticness FLOAT,
         Instrumentalness FLOAT,
         Liveness FLOAT,
         Valence FLOAT,
-        Tempo FLOAT,
         Popularity INT,
         Genre VARCHAR(255)
     );
@@ -65,9 +110,9 @@ def create_database(df):
     for _, row in df.iterrows():
         c.execute(
             """
-            INSERT INTO music_data (Title, Artist, Year, Duration, Time_Signature, Danceability, Energy, `Key`, Loudness, 
-            Loudness_dB, Mode, Speechiness, Acousticness, Instrumentalness, Liveness, Valence, Tempo, Popularity, Genre)
-            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+            INSERT INTO music_data (Title, Artist, Year, Duration, Time_Signature, `Key`, Mode, Camelot_Key, Tempo, Danceability, Energy, Loudness, 
+            Loudness_dB, Speechiness, Acousticness, Instrumentalness, Liveness, Valence, Popularity, Genre)
+            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
             """,
             (
                 row["Track"], # renamed to Title
@@ -75,18 +120,19 @@ def create_database(df):
                 row["Year"],
                 row["Duration"],
                 row["Time_Signature"],
+                row["Key"],
+                row["Mode"],
+                row["Camelot_Key"],
+                row["Tempo"],
                 row["Danceability"],
                 row["Energy"],
-                row["Key"],
                 row["Loudness"],
                 row["Loudness_dB"],
-                row["Mode"],
                 row["Speechiness"],
                 row["Acousticness"],
                 row["Instrumentalness"],
                 row["Liveness"],
                 row["Valence"],
-                row["Tempo"],
                 row["Popularity"],
                 row["Genre"],
             ),
@@ -106,8 +152,16 @@ def print_first_rows(n: int=5):
 
 def main():
     df = pd.read_csv(CSV_FILE)
-    df_normal = normalize_features(df, save_to_csv=True) # Normalize the features
+    df2 = add_camelot_key(df) # Add the Camelot Key column
+    df_normal = normalize_features(df2) # Normalize the features
+    
+    # Reorder DataFrame columns to match the INSERT statement
+    df_normal = df_normal[['Track', 'Artist', 'Year', 'Duration', 'Time_Signature', 'Key', 'Mode', 'Camelot_Key', 
+                           'Tempo', 'Danceability', 'Energy', 'Loudness', 'Loudness_dB', 'Speechiness', 
+                           'Acousticness', 'Instrumentalness', 'Liveness', 'Valence', 'Popularity', 'Genre']]
+    df_normal.to_csv(os.path.join(PROCESSED_CSV_FILE), index=False)
     create_database(df_normal) # Create the database and insert the data
+
     print_first_rows()
 
 if __name__ == "__main__":
