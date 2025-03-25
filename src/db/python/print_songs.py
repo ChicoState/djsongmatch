4906b9b487acf45dfada8260d4ebfa09ac57e801
Lawjay Lee <lawjaylee1@icloud.com>
2025-03-24 21:01:27 -0700

    Add print camelot key table debug file

--- src/db/python/print_songs.py
+++ src/db/python/print_songs.py
@@ -1,6 +1,6 @@
 import sqlalchemy
 from database import DEFAULT_DATABASE_URL
-from models import Song
+from models import Camelot_Key, Song
 
 
 def print_songs():
@@ -14,5 +14,20 @@ def print_songs():
         print(song)
 
 
+def print_camelot_keys():
+    """Debug function to print the camelot keys in the database
+    (Makes sure data is there)
+    """
+    engine = sqlalchemy.create_engine(DEFAULT_DATABASE_URL)
+    session = sqlalchemy.orm.sessionmaker(bind=engine)()
+    camelot_keys = session.query(Camelot_Key).all()
+    for camelot_key in camelot_keys:
+        print(camelot_key)
+
+
 if __name__ == "__main__":
+    print("Printing songs")
     print_songs()
+
+    print("\nPrinting camelot keys")
+    print_camelot_keys()
