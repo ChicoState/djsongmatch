3ad656706e553595cfc6dc1e385d3336565dd92a
Lawjay Lee <lawjaylee1@icloud.com>
2025-03-27 12:06:12 -0700

    Remove missing db file log, incorrect path

--- frontend/db/index.ts
+++ frontend/db/index.ts
@@ -5,9 +5,5 @@ import { existsSync } from 'fs';
 // This path is relative to `drizzle.config.ts`, not current file
 const DB_FILE_NAME = 'file:../db.db'; // Bad practice, use ENV variables in the future
 
-if (!existsSync(DB_FILE_NAME)) {
-    console.log("ERROR: DB file doesn't exist");
-}
-
 // variable to hold the db instance
 export const db = drizzle(DB_FILE_NAME);
