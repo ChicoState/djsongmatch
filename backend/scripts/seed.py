3d69514d8b7f3f91cc0d1d34faacf8fed5bf6509
riley-1995 <rileydenn@gmail.com>
2025-03-27 04:49:33 -0700

    Updated seed.py file to reorder import statements

--- backend/scripts/seed.py
+++ backend/scripts/seed.py
@@ -1,10 +1,9 @@
 import pandas as pd
 from pathlib import Path
-from api.extensions import db
+from api import create_app
 from api.database.operations import init_camelot_keys, insert_songs
 from api.database.management import reset_db
 from api.models import Song
-from api import create_app
 
 PROJECT_ROOT = Path(__file__).parent.parent
 PROCESSED_CSV_PATH = PROJECT_ROOT / "assets" / "Processed_ClassicHit.csv"
