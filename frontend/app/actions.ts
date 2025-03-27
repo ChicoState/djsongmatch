0072cea033f892a958d45b884b1c6fdb39a47a99
Lawjay Lee <lawjaylee1@icloud.com>
2025-03-27 10:46:02 -0700

    Removes getSameKey() fn

--- frontend/app/actions.ts
+++ frontend/app/actions.ts
@@ -19,37 +19,6 @@ export async function getSong(songId: number) {
     return song;
 }
 
-export async function getSameKey(song: Song, relativeKey: boolean = true) {
-    if (song.key === null || song.mode === null) return;
-
-    let relativeQuery = relativeKey
-        ? and(
-            ne(musicData.mode, song.mode),
-
-            // if song is major, go back 3 half steps to get relative minor
-            // if song is minor, go forward 3 half steps to get relative major
-            // mod 12 because 12 total tones
-            eq(musicData.key, (song.mode === MAJOR ? song.key - 3 : song.key + 3) % 12)
-        )
-        : undefined; // if relativeKey=false, just ignore query
-
-    return db
-        .select()
-        .from(musicData)
-        .where(
-            or(
-                and(
-                    // checks that same song and mode
-                    eq(musicData.mode, song.mode),
-                    eq(musicData.key, song.key)
-                ),
-                relativeQuery
-            )
-        )
-        .orderBy(sql`RANDOM()`)
-        .limit(10); // arbitary limit
-}
-
 export async function searchSongs(query: string) {
     const words = query.split(" ");
     const conditions = words.map((word) => {
