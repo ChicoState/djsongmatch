146370b6df2617cd2885978104579deb1fc44637
Lawjay Lee <lawjaylee1@icloud.com>
2025-04-01 16:01:07 -0700

    Content on page from flask

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,44 @@
 "use client";
 import "@/app/globals.css";
 
-import { Song } from "@/db/schema";
+import type { Song } from "@/db/schema";
+import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import ButtonSliderSection from "./_components/ButtonSliderSection";
 import SearchBarSection from "./_components/SearchBarSection";
+import { getRecommendedSongs } from "./actions";
 
 export default function App() {
   const [inputSong, setInputSong] = useState<Song | null>(null);
+
+  const {
+    data: recommendedSongs,
+    isError,
+    isLoading,
+    isSuccess,
+    error,
+  } = useQuery({
+    queryKey: ["recSongs", inputSong?.songId],
+    queryFn: async () => {
+      if (!inputSong) {
+        console.log("WARNING: No input song");
+        return null;
+      }
+
+      return getRecommendedSongs(inputSong.songId);
+    },
+    enabled: !!inputSong,
+  });
+
   return (
     <main className="flex flex-col justify-center items-center">
       <ButtonSliderSection inputSong={inputSong} />
       <SearchBarSection setInputSong={setInputSong} />
+      {isError && <div>Error: {error.message}</div>}
+      {isLoading && <div>Fetching from flask. Loading...</div>}
+      {isSuccess && (
+        <div>Response from flask: "{recommendedSongs?.message}"</div>
+      )}
     </main>
   );
 }
