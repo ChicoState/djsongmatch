e6372b8996dd0a06b72dae569cc7e6343e92ae96
Lawjay Lee <lawjaylee1@icloud.com>
2025-04-01 15:59:36 -0700

    Makes drizzle schema nonnull

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,26 +1,26 @@
-import { sqliteTable, AnySQLiteColumn, integer, real, text } from "drizzle-orm/sqlite-core"
-import { InferSelectModel, sql } from "drizzle-orm"
+import type { InferSelectModel } from "drizzle-orm";
+import { integer, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const musicData = sqliteTable("music_data", {
-    songId: integer("SongID").primaryKey({ autoIncrement: true }),
-    title: text("Title", { length: 255 }),
-    artist: text("Artist", { length: 255 }),
-    year: integer("Year"),
-    duration: integer("Duration"),
-    timeSignature: text("Time_Signature", { length: 10 }),
-    danceability: real("Danceability"),
-    energy: real("Energy"),
-    key: integer("Key"),
-    loudness: real("Loudness"),
-    mode: integer("Mode"),
-    speechiness: real("Speechiness"),
-    acousticness: real("Acousticness"),
-    instrumentalness: real("Instrumentalness"),
-    liveness: real("Liveness"),
-    valence: real("Valence"),
-    tempo: real("Tempo"),
-    popularity: integer("Popularity"),
-    genre: text("Genre", { length: 255 }),
+  songId: integer("SongID").primaryKey({ autoIncrement: true }).notNull(),
+  title: text("Title", { length: 255 }).notNull(),
+  artist: text("Artist", { length: 255 }).notNull(),
+  year: integer("Year").notNull(),
+  duration: integer("Duration").notNull(),
+  timeSignature: text("Time_Signature", { length: 10 }).notNull(),
+  danceability: real("Danceability").notNull(),
+  energy: real("Energy").notNull(),
+  key: integer("Key").notNull(),
+  loudness: real("Loudness").notNull(),
+  mode: integer("Mode").notNull(),
+  speechiness: real("Speechiness").notNull(),
+  acousticness: real("Acousticness").notNull(),
+  instrumentalness: real("Instrumentalness").notNull(),
+  liveness: real("Liveness").notNull(),
+  valence: real("Valence").notNull(),
+  tempo: real("Tempo").notNull(),
+  popularity: integer("Popularity").notNull(),
+  genre: text("Genre", { length: 255 }).notNull(),
 });
 
 export type Song = InferSelectModel<typeof musicData>;
