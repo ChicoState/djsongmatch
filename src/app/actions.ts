0271716de39a97345af97b703b2782c36f230c03
Lawjay Lee <lawjaylee1@icloud.com>
2025-04-01 16:00:52 -0700

    Adds server action for flask

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,73 +1,69 @@
 "use server";
-// entire file uses server ACTIONS, 
+// entire file uses server ACTIONS,
 // do NOT export components in this file, only functions
 
-import { musicData, Song } from "@/db/schema";
-import { and, eq, like, ne, or, sql } from "drizzle-orm";
+import { musicData } from "@/db/schema";
+import { and, eq, like, or } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/libsql";
 
 const db = drizzle("file:./assets/ClassicHit.db");
 
-// constants for song.mode
-const MINOR = 0;
-const MAJOR = 1
-
 export async function getSong(songId: number) {
-    const song = db
-        .select()
-        .from(musicData)
-        .where(
-            eq(musicData.songId, Number(songId.toString()))
-        ).get();
+  const song = db
+    .select()
+    .from(musicData)
+    .where(eq(musicData.songId, Number(songId.toString())))
+    .get();
 
-    return song;
-}
-
-export async function getSameKey(song: Song, relativeKey: boolean = true) {
-    if (song.key === null || song.mode === null) return;
-
-    let relativeQuery = relativeKey
-        ? and(
-            ne(musicData.mode, song.mode),
-
-            // if song is major, go back 3 half steps to get relative minor
-            // if song is minor, go forward 3 half steps to get relative major
-            // mod 12 because 12 total tones
-            eq(musicData.key, (song.mode === MAJOR ? song.key - 3 : song.key + 3) % 12)
-        )
-        : undefined; // if relativeKey=false, just ignore query
-
-    return db
-        .select()
-        .from(musicData)
-        .where(
-            or(
-                and(
-                    // checks that same song and mode
-                    eq(musicData.mode, song.mode),
-                    eq(musicData.key, song.key)
-                ),
-                relativeQuery
-            )
-        )
-        .orderBy(sql`RANDOM()`)
-        .limit(10); // arbitary limit
+  return song;
 }
 
 export async function searchSongs(query: string) {
-    const words = query.split(" ");
-    const conditions = words.map((word) => {
-        return or(
-            like(musicData.title, `%${word}%`),
-            like(musicData.artist, `%${word}%`)
-        );
-    });
+  const words = query.split(" ");
+  const conditions = words.map((word) => {
+    return or(
+      like(musicData.title, `%${word}%`),
+      like(musicData.artist, `%${word}%`),
+    );
+  });
 
-    return db
-        .select()
-        .from(musicData)
-        .where(
-            and(...conditions)
-        )
-        .limit(100)
+  return db
+    .select()
+    .from(musicData)
+    .where(and(...conditions))
+    .limit(100);
+}
+
+/*
+ * In the future, this will define how the response from flask should look like (JSON)
+ */
+interface getRecommendedSongsResponse {
+  message: string;
+}
+
+export async function getRecommendedSongs(
+  songId: number,
+): Promise<getRecommendedSongsResponse> {
+  // what to give to flask
+  const flaskParams = {
+    songId: songId.toString(),
+  };
+
+  // url to flask server
+  const flaskUrl = new URL("http://127.0.0.1:5001");
+  flaskUrl.search = new URLSearchParams(flaskParams).toString();
+
+  console.log(flaskUrl);
+  console.log(`Fetching recommendations for songId: ${songId} from flask!`);
+  try {
+    const response = await fetch(flaskUrl);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  } catch (error) {
+    throw new Error(
+      "Error fetching recommendations. Is the flask server running?",
+    );
+  }
 }
