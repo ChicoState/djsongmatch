d5ec705f85868ab1b174bcdceaf43be62196b6a8
riley-1995 <rileydenn@gmail.com>
2025-04-03 03:17:55 -0700

    Add config.py to separate out flask config logic from api/__init__.py. Update docker-compose.yml to mount .env to containers. Update backend/Dockerfile environment variables and take away copying the entire backend into the image since it's mounted via docker-compose volumes. This also improves security by not baking any secrets into a Docker image.

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,42 +1,27 @@
-import os
-from pathlib import Path
-
-from dotenv import load_dotenv
 from flask import Flask
-
+from config import config
 from api.extensions import db
 
-from .routes.camelot_keys import camelot_keys_bp
-from .routes.songs import songs_bp
-
-
-# def create_app(config_object="settings.py"): # can create settings.py
-def create_app():
+def create_app(config_name="default"):
     """Application factory pattern"""
     app = Flask(__name__)
 
-    # Configure
-    # app.config.from_pyfile(config_object) # add if using a settings.py file
-    BASE_DIR = Path(__file__).parents[2]
-
-    # Source .env file
-    load_dotenv(BASE_DIR / ".env")
-
-    USER = os.getenv("user")
-    PASSWORD = os.getenv("password")
-    HOST = os.getenv("host")
-    PORT = os.getenv("port")
-    DBNAME = os.getenv("dbname")
-
-    DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    # Configure from config.py
+    app.config.from_object(config[config_name])
 
     # Initialize extensions
     db.init_app(app)
 
     # Register blueprints
+    from .routes.camelot_keys import camelot_keys_bp
+    from .routes.songs import songs_bp
+
     app.register_blueprint(songs_bp, url_prefix="/api/songs")
     app.register_blueprint(camelot_keys_bp, url_prefix="/api/camelot_keys")
+
+    # Late initialization of models
+    with app.app_context():
+        from api.database.models import Song, CamelotKey
+        db.create_all()
 
     return app
