f39915a26db4e7b1262350ac8f6edb58040584ab
Lawjay Lee <lawjaylee1@icloud.com>
2025-04-09 20:14:16 -0700

    Adds TableSection

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import "@/app/globals.css";
 
-import { Song } from "@/db/schema";
+import type { Song } from "@/db/schema";
 import { useState } from "react";
 import ButtonSliderSection from "./_components/ButtonSliderSection";
 import SearchBarSection from "./_components/SearchBarSection";
+import TableSection from "./_components/TableSection";
 
 export default function App() {
   const [inputSong, setInputSong] = useState<Song | null>(null);
@@ -12,6 +13,7 @@ export default function App() {
     <main className="flex flex-col justify-center items-center">
       <ButtonSliderSection inputSong={inputSong} />
       <SearchBarSection setInputSong={setInputSong} />
+      <TableSection />
     </main>
   );
 }
