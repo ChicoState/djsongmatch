2be9c7681d01907d4133c4d0cc661d6045d76d36
Lawjay Lee <lawjaylee1@icloud.com>
2025-04-10 21:02:19 -0700

    Temporarily removes MoveRight icon

--- frontend/app/_components/TableSection.tsx
+++ frontend/app/_components/TableSection.tsx
@@ -1,4 +1,3 @@
-import { MoveRightIcon } from "lucide-react";
 import PlaylistTable from "./PlaylistTable";
 import RecommendationTable from "./RecommendationTable";
 
@@ -6,7 +5,6 @@ export default function TableSection() {
   return (
     <div className="flex gap-4 px-16 w-full h-full">
       <RecommendationTable />
-      <MoveRightIcon className="self-center" />
       <PlaylistTable />
     </div>
   );
