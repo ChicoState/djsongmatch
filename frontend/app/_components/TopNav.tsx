b10f7a302014f3834be465230634a5f99c4a0faf
smartinez1123 <srmartinezbarajas@csuchico.edu>
2025-04-21 12:16:27 -0700

    Added the side tab for the settings button

--- frontend/app/_components/TopNav.tsx
+++ frontend/app/_components/TopNav.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
-import DarkModeToggle from "./DarkModeToggle";
 import Help from "./Help";
-import { SettingsIcon } from "lucide-react";
+import SettingsDrawer from "./SettingsDrawer"; // import the drawer
 
 export default function TopNav() {
   return (
@@ -10,12 +9,13 @@ export default function TopNav() {
       <nav className="flex justify-between items-center p-4 font-semibold border-b border-border">
         <h1 className="text-3xl">DJ Song Match</h1>
         <div className="flex gap-4 items-center">
-        <Button variant="default">Export</Button>
+          <Button variant="default">Export</Button>
           <Help />
           <Button variant="outline">Log in</Button>
-	  <SettingsIcon size={32} className="text-secondary-foreground"/>
+          <SettingsDrawer />
         </div>
       </nav>
     </div>
   );
 }
+
