a7a26383c3488944cd3b8c947addf2c4b6924872
Madison Webb <madisonewebb28@gmail.com>
2025-04-21 12:34:35 -0700

    ESLint is working for frontend, run 'pnpm lint' in fronted dir

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      ecmaFeatures: { jsx: true },
+    },
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'next/core-web-vitals'
+    ],
+    rules: {
+      // add custom rules here
+    }
+  };
+  
