166a27d3d540dad8abd3231f3c19f010f74de3e4
Lawjay Lee <lawjaylee1@icloud.com>
2025-04-28 15:36:52 -0700

    Use hooks for localStorage and no context

--- frontend/lib/hooks.ts
+++ frontend/lib/hooks.ts
@@ -1,6 +1,9 @@
 "use client";
 
+import { Song } from "@/db/schema";
+import { useLocalStorage } from "@uidotdev/usehooks";
 import { useEffect, useState } from "react";
+import { SongWithUuid } from "./utils";
 
 export function useDebounce(value: any, delay: number) {
   /**
@@ -20,4 +23,20 @@ export function useDebounce(value: any, delay: number) {
     };
   }, [value, delay]);
   return debounceValue;
+}
+
+export function useSelectedSong() {
+  const [selectedSong, setSelectedSong] = useLocalStorage<Song | undefined>(
+    "selectedSong",
+    undefined,
+  );
+  return { selectedSong, setSelectedSong };
+}
+
+export function usePlaylist() {
+  const [playlist, setPlaylist] = useLocalStorage<SongWithUuid[]>(
+    "playlist",
+    [],
+  );
+  return { playlist, setPlaylist };
 }
